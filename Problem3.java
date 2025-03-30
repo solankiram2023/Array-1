ed25e8bd096f35a5fd86dcb557d47a540d98a304
solankiram2023 <145072212+solankiram2023@users.noreply.github.com>
2025-03-30 07:43:14 -0400

    P3

---
+++ Problem3.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Problem3 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int VISITED = 101;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        // Four directions that we will move: right, down, left, up.
+        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        // Initial direction: moving right.
+        int currentDirection = 0;
+        // The number of times we change the direction.
+        int changeDirection = 0;
+        // Current place that we are at is (row, col).
+        // row is the row index; col is the column index.
+        int row = 0;
+        int col = 0;
+        // Store the first element and mark it as visited.
+        List<Integer> result = new ArrayList<>();
+        result.add(matrix[0][0]);
+        matrix[0][0] = VISITED;
+        while (changeDirection < 2) {
+            while (
+                row + directions[currentDirection][0] >= 0 &&
+                row + directions[currentDirection][0] < rows &&
+                col + directions[currentDirection][1] >= 0 &&
+                col + directions[currentDirection][1] < columns &&
+                matrix[row + directions[currentDirection][0]][col +
+                        directions[currentDirection][1]] !=
+                    VISITED
+            ) {
+                // Reset this to 0 since we did not break and change the direction.
+                changeDirection = 0;
+                // Calculate the next place that we will move to.
+                row = row + directions[currentDirection][0];
+                col = col + directions[currentDirection][1];
+                result.add(matrix[row][col]);
+                matrix[row][col] = VISITED;
+            }
+            // Change our direction.
+            currentDirection = (currentDirection + 1) % 4;
+            // Increment change_direction because we changed our direction.
+            changeDirection++;
+        }
+        return result;
+    }
+}
